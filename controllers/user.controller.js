f6958840050cb543e3cfbbcd77649a3c181d4c50
naval kumar <nawalkumar4810167@gmail.com>
2026-02-27 03:02:09 +0530

    adding cookies action

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -134,7 +134,8 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "Strict",
+        secure: false,
+        sameSite: "None",
       })
       .json({
         message: `Welcome back ${user.fullname}`,
